7fc33c449606c081e9a3388fd3c230279a8e811c
João Victor dos Santos Mendes <jvictorsantos852@gmail.com>
2023-07-21 23:03:55 -0300

    fix: projects not being saved

--- src/services/store/index.ts
+++ src/services/store/index.ts
@@ -1,15 +1,12 @@
+import { STORE_KEYS } from '../../constants/store'
 import Project from '../../models/Project'
 import store from './config'
 import { ProjectSchema } from './schema'
 
 export function getAllProjects(): Project[] {
-  const projectsData = store.get('project') as ProjectSchema[]
+  const projectsData = store.get(STORE_KEYS.PROJECTS) as ProjectSchema[]
 
-  if (!projectsData) {
-    return []
-  }
-
-  return projectsData as Project[]
+  return (projectsData ?? []) as Project[]
 }
 
 export function saveProject(data: { name: string; path: string }): void {
@@ -18,13 +15,13 @@ export function saveProject(data: { name: string; path: string }): void {
     path: data.path,
   })
 
-  const projectsData = store.get('project') as ProjectSchema[]
+  const projectsData = (store.get(STORE_KEYS.PROJECTS) ?? []) as ProjectSchema[]
 
-  store.set('project', [...projectsData, project])
+  store.set(STORE_KEYS.PROJECTS, [...projectsData, project])
 }
 
 export function getProject(id: string): Project {
-  const projectsData = store.get('project') as ProjectSchema[]
+  const projectsData = (store.get(STORE_KEYS.PROJECTS) ?? []) as ProjectSchema[]
 
   const project = projectsData.find((project: Project) => project.id === id)
 
@@ -36,7 +33,7 @@ export function getProject(id: string): Project {
 }
 
 export function updateProject(id: string, data: Partial<Omit<Project, 'id'>>): Project {
-  const projectsData = store.get('project') as ProjectSchema[]
+  const projectsData = (store.get(STORE_KEYS.PROJECTS) ?? []) as ProjectSchema[]
 
   const project = projectsData.find((project: Project) => project.id === id)
 
@@ -58,13 +55,13 @@ export function updateProject(id: string, data: Partial<Omit<Project, 'id'>>): P
     return project
   })
 
-  store.set('project', updatedProjectsData)
+  store.set(STORE_KEYS.PROJECTS, updatedProjectsData)
 
   return updatedProject as Project
 }
 
 export function deleteProject(id: string): void {
-  const projectsData = store.get('project') as ProjectSchema[]
+  const projectsData = (store.get(STORE_KEYS.PROJECTS) ?? []) as ProjectSchema[]
 
   const project = projectsData.find((project: Project) => project.id === id)
 
@@ -74,5 +71,5 @@ export function deleteProject(id: string): void {
 
   const updatedProjectsData = projectsData.filter((project: Project) => project.id !== id)
 
-  store.set('project', updatedProjectsData)
+  store.set(STORE_KEYS.PROJECTS, updatedProjectsData)
 }
