53f716ef0c39e5cba0b837bb0d08e6baf2045fb3
João Victor <jvictorsantos852@gmail.com>
2026-03-10 18:39:02 -0300

    fix: configure prettier as default formatter and update middleware matcher

--- apps/docs/middleware.ts
+++ apps/docs/middleware.ts
@@ -5,5 +5,5 @@ export default createI18nMiddleware(i18n)
 
 export const config = {
   // Matcher ignoring `/_next/` and `/api/`
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|og).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|og|.*\\..*).*)'],
 }
